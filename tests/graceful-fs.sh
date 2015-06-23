2ad562cc8ded90f7fb6244dde3279a787baf55e9
Rod Vagg <rod@vagg.org>
2015-06-23 11:50:04 +1000

    added lodash and graceful-fs

--- tests/graceful-fs.sh
+++ tests/graceful-fs.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/isaacs/node-graceful-fs.git
+git clone https://github.com/isaacs/node-graceful-fs.git --depth 1
 cd node-graceful-fs
-npm install --cache-min=9999
+npm install --cache-min=Infinity
 npm test
---
+++ tests/lodash.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/lodash/lodash.git --depth 1
+cd lodash
+npm install --cache-min=Infinity
+node test/test.js
