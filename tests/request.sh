f7b3904cae3e9dadd6635966fff711a0b76ea1bb
Rod Vagg <rod@vagg.org>
2015-06-23 15:23:23 +1000

    extend request timeouts

--- tests/request.sh
+++ tests/request.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/request/request.git --depth 1
 cd request
 npm install --cache-min=Infinity
-npm test
+sed -i.bak 's/taper /taper --timeout 120000 /g' package.json
+npm run test-ci
